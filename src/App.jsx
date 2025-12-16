f0c150288d09e0f4d1e177d0be9916ba99839c13
Juamp11 <jpborello25@gmail.com>
2025-12-15 23:01:50 -0300

    fix: suppress react router v7 warning

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Admin from './pages/Admin';
 
 function App() {
     return (
-        <Router>
+        <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/admin" element={<Admin />} />
