f3213e029ddb0bb7d6813dae0a749502799c108f
Juamp11 <jpborello25@gmail.com>
2025-12-17 02:45:27 -0300

    chore: Add Open Graph Image for Social Sharing

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -23,13 +23,16 @@ const SEO = ({
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
             <meta property="og:site_name" content={name} />
-            {/* <meta property="og:image" content={image} />  TODO: Add default social image */}
+            <meta property="og:image" content="https://ferreyraww.vercel.app/ferreyra-board-logo.png" />
+            <meta property="og:image:width" content="1200" />
+            <meta property="og:image:height" content="630" />
 
             {/* Twitter */}
             <meta name="twitter:creator" content={name} />
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:title" content={title} />
             <meta name="twitter:description" content={description} />
+            <meta name="twitter:image" content="https://ferreyraww.vercel.app/ferreyra-board-logo.png" />
         </Helmet>
     );
 }
